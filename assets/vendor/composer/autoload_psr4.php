ca1dab375190dc24567c52f8fe4ae4e6ecef9ba8
Alexandr Borisov <sasha@ubuntuSasha>
2017-05-30 12:08:29 +0300

    some changes (rebuild slim sceleton)

--- assets/vendor/composer/autoload_psr4.php
+++ assets/vendor/composer/autoload_psr4.php
@@ -7,5 +7,9 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Slim\\Views\\' => array($vendorDir . '/slim/php-view/src'),
+    'Slim\\' => array($vendorDir . '/slim/slim/Slim'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
+    'Psr\\Container\\' => array($vendorDir . '/psr/container/src'),
+    'Interop\\Container\\' => array($vendorDir . '/container-interop/container-interop/src/Interop/Container'),
+    'FastRoute\\' => array($vendorDir . '/nikic/fast-route/src'),
 );
--- assets/vendor/composer/autoload_real.php
+++ assets/vendor/composer/autoload_real.php
@@ -40,6 +40,20 @@ class ComposerAutoloaderInit3dfd8387a73fda48e03b189ca8099186
 
         $loader->register(true);
 
+        $includeFiles = require __DIR__ . '/autoload_files.php';
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire3dfd8387a73fda48e03b189ca8099186($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire3dfd8387a73fda48e03b189ca8099186($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
